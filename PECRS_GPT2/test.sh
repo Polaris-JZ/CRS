ba877f7cceedc2038449a545d9a8cb9a90fef46d
Polaris-JZ <112391065+Polaris-JZ@users.noreply.github.com>
2025-02-21 15:28:41 +0100

    update

---
+++ PECRS_GPT2/test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH -J um-n-redail
+#SBATCH -p gpu_a100
+#SBATCH --cpus-per-task=18
+#SBATCH --gpus=1
+#SBATCH --time=5-00:00:00
+#SBATCH --output=train.txt  # 标准输出文件
+
+# 激活 conda 环境
+source ~/.bashrc
+conda activate torch2.1.1
+
+srun --gres=gpu:1 \
+    python main.py \
+    --mode=eval \
+    --load_model_path=/data/zhaoj9/KG_repro/PLM_based/redail/efficient_unified_crs/Outputs/REDIAL/temp/CRS_Train_best_model.pt \
+                              
